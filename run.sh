0f8974bcfd1dd9fd7f710137a436ebd725daeb98
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-05-20 08:27:06 +0530

    Kunwar fixed the bot looping stuff It was messing with the supervisor

--- run.sh
+++ run.sh
@@ -3,16 +3,10 @@
 # Get to a predictable directory, the directory of this script
 cd "$(dirname "$0")"
 
+source /root/TLE/venv/bin/activate
+
 [ -e secrets ] && . ./secrets
 
-while true; do
-    git pull
-    python -m pip install -r requirements.txt
-    FONTCONFIG_FILE=$PWD/extra/fonts.conf python __main__.py
-
-    (( $? != 42 )) && break
-
-    echo '==================================================================='
-    echo '=                       Restarting                                ='
-    echo '==================================================================='
-done
+git pull
+python -m pip install -r requirements.txt
+FONTCONFIG_FILE=$PWD/extra/fonts.conf python __main__.py
